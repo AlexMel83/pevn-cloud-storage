832c3e147161d8a6a5fe5252e140fed6c235e377
Alexander Meleshko <meleshko.a.n@ukr.net>
2024-03-25 17:08:52 +0200

    +

--- server/sql/knexfile.js
+++ server/sql/knexfile.js
@@ -1,28 +1,45 @@
-requestAnimationFrame('dotenv').config({
-    path: `${__dirname}/../.${
-        process.env.NODE_ENV === "development" ? process.env.NODE_ENV : "production"
-    }.env`,
-});
-
 /**
- * @type {Object.<string, import('knex').Knex.Config>}
+ * @type { Object.<string, import("knex").Knex.Config> }
  */
-
 module.exports = {
-    development: {
-        client: "postgresql",
-        connection:{
-            host: process.env.POSTGRES_HOST,
-            port: process.env.POSTGRES_PORT,
-            database: process.env.POSTGRES_DB,
-            user: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            debug: true
-        },
-        pool: { 
-            min: 0,
-            max: 7
-        },
-        seeds: { directory: "./seeds/development"}
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
     }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
 };

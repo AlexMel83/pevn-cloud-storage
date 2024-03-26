9cf6da946133f5e8544253d36e3b4ee9043b820e
Alexander Meleshko <meleshko.a.n@ukr.net>
2024-03-26 12:07:24 +0200

    +

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,14 +1,14 @@
 const knex = require("./../config/knex.config");
 const { validationResult } = require("express-validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const uuid = require("uuid");
 const userModel = require('./../models/user-model');
-const mailService = require("./../services/mail-service");
+const UserDto = require("./../dtos/user-dto");
+// const mailService = require("./../services/mail-service");
 const ApiError = require("./../exceptions/api-errors");
 
 class UserController {
     async registration(req, res, next){
-        const {email, password, name, surname, phone} = req.body;
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
@@ -16,44 +16,42 @@ class UserController {
                 ApiError.BadRequest("Помилка при валідації", errors.array()),
             );
         }
+
+        const userDto = new UserDto(req.body);
         
         const trx = await knex.transaction();
         try{
-            const payload = {
-                email, name, surname, phone,
+            const candidate = await userModel.findUserByEmail(userDto.email, trx);
+            if(candidate.length){
+                return res.status(400).send(ApiError.BadRequest(`Email ${userDto.email} already exists`));
             }
 
-            const candidate = await userModel.findUserByEmail(email, trx);
-            if(candidate){
-                console.log(candidate)
-            }
+            userDto.password = await bcrypt.hash(req.body.password, 15);
+            userDto.activationlink = uuid.v4();
 
-            payload.password = await bcrypt.hash(password, 15);
-            payload.activationlink = uuid.v4();
+            const user = await userModel.insertUser(userDto, trx);
+            user[0].password = "secure";
 
-            const user = await userModel.insertUser(payload, trx);
-
-            if (process.env.NODE_ENV === "development") {
-                await mailService.sendActivationMail(
-                  email,
-                  `${API_URL}:${PORT}/activate/${activationlink}`,
-                );
-              } else {
-                await mailService.sendActivationMail(
-                  email,
-                  `${API_URL}/activate/${activationlink}`,
-                );
-              }
+            // if (process.env.NODE_ENV === "development") {
+            //     await mailService.sendActivationMail(
+            //       email, `${API_URL}:${PORT}/activate/${activationlink}`,
+            //     );
+            //   } else {
+            //     await mailService.sendActivationMail(
+            //       email, `${API_URL}/activate/${activationlink}`,
+            //     );
+            //   }
 
             await trx.commit();
             return res.status(200).json(user);
         } catch(error){
             await trx.rollback();
             if (error.code === "23505") {
-                return res.status(400).send(ApiError.BadRequest(`Email ${email} already exists`));
+                return res.status(400).send(ApiError.BadRequest(`Email ${userDto.email} already exists`));
               } else if (error.code === "42P01") {
-                return res.status(400).send(ApiError.BadRequest("Table 'users' does not exist"));
+                return res.status(500).send(ApiError.IntServError("Table 'users' does not exist"));
               } else {
+                console.error(error);
                 return res.status(500).send(ApiError.IntServError(error.detail));
               }
         }
---
+++ server/dtos/user-dto.js
@@ -0,0 +1,21 @@
+module.exports = class UserDto {
+    id;
+    email;
+    name;
+    surname;
+    phone;
+    role;
+    avatar;
+
+  
+    constructor(model) {
+      this.id = model.id;
+      this.email = model.email;
+      this.name = model.name;
+      this.surname = model.surname;
+      this.phone = model.phone;
+      this.role = model.role;
+      this.avatar = model.avatar;
+    }
+  };
+  

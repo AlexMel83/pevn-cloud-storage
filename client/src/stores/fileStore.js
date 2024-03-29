2e6e811e0ccd7d5f4aa079b054804d3636e58bd0
Alexander Meleshko <meleshko.a.n@ukr.net>
2024-03-29 12:45:50 +0200

    +

---
+++ client/src/stores/fileStore.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useFileStore = defineStore('file', {
+  state: () => ({
+    // ваше начальное состояние
+  }),
+  getters: {
+    // ваше состояние может также включать геттеры (аналогично селекторам в Redux)
+  },
+  actions: {
+    // здесь вы можете добавлять методы для обновления состояния
+  }
+})
